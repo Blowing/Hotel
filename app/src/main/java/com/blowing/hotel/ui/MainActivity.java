206af0427842928f1c31d10565a090a2a758d0d0
Blowing <1072878103@qq.com>
2019-11-21 23:45:17 +0800

    完成主页的UI编写

--- app/src/main/java/com/blowing/hotel/ui/MainActivity.java
+++ app/src/main/java/com/blowing/hotel/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.blowing.hotel;
+package com.blowing.hotel.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.blowing.hotel.R;
 
 public class MainActivity extends AppCompatActivity {
 
