2aef5da47b2e9a7aa84d093a6bdcd40d4018b2bb
Blowing <1072878103@qq.com>
2019-11-23 22:54:07 +0800

    添加地图的显示

--- app/src/main/java/com/blowing/hotel/ui/HotelActivity.java
+++ app/src/main/java/com/blowing/hotel/ui/HotelActivity.java
@@ -3,33 +3,49 @@ package com.blowing.hotel.ui;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 import com.blowing.hotel.R;
 import com.blowing.hotel.ui.adapter.PictureAdapeter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HotelActivity extends AppCompatActivity {
+public class HotelActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewPager mViewPager;
     private ProgressBar mSeekBar;
     private PictureAdapeter pictureAdapeter;
+    private MapView mMapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
         initView();
         initPictureAdapter();
+        initMapView(savedInstanceState);
     }
 
     private void initView() {
         mViewPager = findViewById(R.id.hotel_vp_bg);
         mSeekBar = findViewById(R.id.hotel_seekbar_picturs);
+        findViewById(R.id.hotel_lv_picture).setOnClickListener(this);
+        findViewById(R.id.hotel_rv_map).setOnClickListener(this);
+    }
 
+    private void initMapView(Bundle savedInstanceState) {
+         mMapView = (MapView) findViewById(R.id.hotel_map);
+        mMapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mMapView.getMap();
+
+        aMap.setTrafficEnabled(true);// 显示实时交通状况
+        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
+        aMap.setMapType(AMap.MAP_TYPE_NORMAL);// 卫星地图模式
     }
 
     private void initPictureAdapter() {
@@ -83,5 +99,19 @@ public class HotelActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.hotel_lv_picture:
+                mViewPager.setVisibility(View.VISIBLE);
+                mMapView.setVisibility(View.GONE);
+                break;
+            case R.id.hotel_rv_map:
+                mMapView.setVisibility(View.VISIBLE);
+                mViewPager.setVisibility(View.GONE);
+                break;
+        }
     }
 }
