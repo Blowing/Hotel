6b518eee2c003ff0de37c28bb000f2fd60926d43
Blowing <1072878103@qq.com>
2019-11-24 15:42:59 +0800

    完成地图的编写

--- app/src/main/java/com/blowing/hotel/ui/HotelActivity.java
+++ app/src/main/java/com/blowing/hotel/ui/HotelActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.blowing.hotel.R;
 import com.blowing.hotel.ui.adapter.HomeAdapter;
 import com.blowing.hotel.ui.adapter.PictureAdapeter;
@@ -103,6 +104,15 @@ public class HotelActivity extends AppCompatActivity implements View.OnClickList
         aMap.setTrafficEnabled(true);// 显示实时交通状况
         //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
         aMap.setMapType(AMap.MAP_TYPE_NORMAL);// 卫星地图模式
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType
+        // (MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        // 连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW);
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false
     }
 
     private void initPictureAdapter() {
--- app/src/main/java/com/blowing/hotel/ui/MainActivity.java
+++ app/src/main/java/com/blowing/hotel/ui/MainActivity.java
@@ -1,24 +1,136 @@
 package com.blowing.hotel.ui;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.blowing.hotel.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AMapLocationListener,
+        AMap.OnMarkerClickListener {
+
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mlocationClient = null;
+    //声明定位回调监听器
+    public AMapLocationClientOption mLocationOption = null;
+
+    private MapView mMapView;
+    private MyLocationStyle myLocationStyle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.main_iv_set).setOnClickListener(new View.OnClickListener() {
+
+        initMapView(savedInstanceState);
+        initLocation();
+        findViewById(R.id.main_rv_location).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,HotelActivity.class);
-                startActivity(intent);
+                startLocation();
             }
         });
+
+
+
+    }
+
+    private void initLocation() {
+
+
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType
+        // (MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        // 连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW);
+        mMapView.getMap().setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        mMapView.getMap().getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        mMapView.getMap().setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false
+        // 表示隐藏定位蓝点并不进行定位，默认是false。
+
+
+        mlocationClient = new AMapLocationClient(this);
+//初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+//设置定位监听
+        mlocationClient.setLocationListener(this);
+//设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+//设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        mLocationOption.setOnceLocation(true);
+
+//获取最近3s内精度最高的一次定位结果：
+//设置setOnceLocationLatest(boolean b)
+// 接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)
+// 接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+        mLocationOption.setNeedAddress(true);
+//设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+// 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+// 在定位结束后，在合适的生命周期调用onDestroy()方法
+// 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+//启动定位
+        mlocationClient.startLocation();
+    }
+
+    private void startLocation() {
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.startLocation();
+        }
+    }
+
+
+    private void initMapView(Bundle savedInstanceState) {
+        mMapView = (MapView) findViewById(R.id.map_view);
+        mMapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mMapView.getMap();
+        aMap.setOnMarkerClickListener(this);
+
+        aMap.setTrafficEnabled(true);// 显示实时交通状况
+        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
+        aMap.setMapType(AMap.MAP_TYPE_NORMAL);// 卫星地图模式
+    }
+
+
+    @Override
+    public void onLocationChanged(AMapLocation location) {
+        if (location != null) {
+            Log.i("wujie", location.getAddress());
+
+            mMapView.getMap().addMarker(new MarkerOptions()
+                    .position(new LatLng(location.getLatitude()+0.005,
+                            location.getLongitude()+0.005))
+                    .icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory
+                            .decodeResource(getResources(),R.mipmap.ic_marker)))
+                    .draggable(true));
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Intent intent = new Intent(MainActivity.this, HotelActivity.class);
+        startActivity(intent);
+        return true;
     }
 }
+
